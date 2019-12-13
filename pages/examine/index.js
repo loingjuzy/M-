79fa6b3a9bea6e1660e71b58d07c48ee2a19617d
loingjuzy <loingjuzy@qq.com>
2019-12-13 17:03:02 +0800

    201912131702

--- pages/examine/index.js
+++ pages/examine/index.js
@@ -5,62 +5,103 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    userStudyInfo: '',
+    userInfo: {
+      "birthday": null,
+      "cratetime": "Thu, 12 Dec 2019 23:34:57 GMT",
+      "education": null,
+      "name": "萌芽的小石头",
+      "occupation": null,
+      "school": null,
+      "studying": 1,
+      "topimage": "/images/01.jpg",
+      "userid": 1
+    },
   },
+  getUserStudy: function() { //获取书本列表信息
+    let that = this;
+    wx.request({
+      url: 'https://www.0752gh.com/getUserStudy',
+      data: {
 
+      },
+      header: { //请求头
+        "Content-Type": "applciation/json"
+      },
+      method: 'GET',
+      dataType: 'json',
+      responseType: 'text',
+      success: function(res) {
+        that.setData({ //如果在sucess直接写this就变成了wx.request()的this了
+          userStudyInfo: res.data
+        });
+        console.log(res.data);
+      },
+      fail: function(err) {
+        // console.log("查询失败");
+      }, //请求失败
+      complete: function() {}, //请求完成后执行的函数
+    })
+  },
+  startStudy: function() {
+    console.log(this.data.userInfo.studying, this.data.userInfo.userid);
+    wx.navigateTo({
+      url: '/pages/remember/index?bookid=' + this.data.userInfo.studying + '&userid=' + this.data.userInfo.userid,
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    this.getUserStudy();
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
--- pages/remember/index.js
+++ pages/remember/index.js
@@ -5,66 +5,105 @@ Page({
    * 页面的初始数据
    */
   data: {
-    answer:1,
+    answer: 1,
+    userStudyInfo:'',
+    studyNum:0,
+    total:0,
+    currentQuestion:0,
   },
-  btnFindanswer:function(){
+  btnFindanswer: function() {
     this.setData({
-      answer:!this.data.answer
+      answer: !this.data.answer
     });
+  },
+  getStudyQuestion: function(options) {
+    let that = this;
+    wx.request({
+      url: 'https://www.0752gh.com/getStudyQuestion',
+      data: options,
+      header: { //请求头
+        "Content-Type": "applciation/json"
+      },
+      method: 'GET',
+      dataType: 'json',
+      responseType: 'text',
+      success: function(res) {
+        that.setData({ //如果在sucess直接写this就变成了wx.request()的this了
+          userStudyInfo: res.data,
+          total: res.data.length
+        });
+        console.log(res.data);
+      },
+      fail: function(err) {
+        // console.log("查询失败");
+      }, //请求失败
+      complete: function() {}, //请求完成后执行的函数
+    })
+  },
+  deleteQuestion:function(){
+    let that = this
+    that.data.userStudyInfo.splice(this.data.currentQuestion, 1)
+    that.setData({ //如果在sucess直接写this就变成了wx.request()的this了
+      userStudyInfo: this.data.userStudyInfo,
+      studyNum:this.data.studyNum+1
+    });
+    console.log(this.data.userStudyInfo);
   },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    this.getStudyQuestion(options)
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
+  onShow: function() {
+   if(this.data.studyNum==15){
+     console.log('你已经学习完成')
+   }
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
